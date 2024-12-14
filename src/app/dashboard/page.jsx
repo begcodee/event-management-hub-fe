46b455a30d46df38da66743fffb5bfdbb1574fef
Paul Hackman <Paulhirohack433@gmail.com>
2024-12-14 17:02:27 +0000

    update

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ComponentName = (props) => {
+    return (
+        <div>
+            <h1>Hello, Samuel!</h1>
+        </div>
+    );
+};
+
+export default ComponentName;
